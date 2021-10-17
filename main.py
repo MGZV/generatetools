bffe5a08984d6fcc10cef48b4e35f249ce10458c
Professional <anr.mgzv@gmail.com>
2021-10-17 21:41:59 +0300

    second commit: add function and while loop

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import random
+
 letters = "АБВГДЕЖИКЛМНОПРСТУФХЦЧШЭЮЯ"
 numbers = "123456789"
 
-letters_cod = ""
-numbers_cod = ""
+cod_list = []
+count = 0
 
-for i in range(0, 3):
-    letters_cod += random.choice(letters)
 
-for i in range(0, 8):
-    numbers_cod += random.choice(numbers)
-    if len(numbers_cod) == 3:
-        numbers_cod += "."
-    if len(numbers_cod) == 6:
-        numbers_cod += "."
+def generate_cod():
+    letters_cod = ""
+    numbers_cod = ""
+    for i in range(0, 3):
+        letters_cod += random.choice(letters)
 
-print(f"{letters_cod} {numbers_cod}")
+    for i in range(0, 8):
+        numbers_cod += random.choice(numbers)
+        if len(numbers_cod) == 3:
+            numbers_cod += "."
+        if len(numbers_cod) == 6:
+            numbers_cod += "."
+    return letters_cod + " " + numbers_cod
+
+
+while count != 5:
+    cod_list.append(generate_cod())
+    count += 1
+
+print(cod_list)
